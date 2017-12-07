33b4f6970dfb18a27879074c213a27d4fd78d939
larrycl <lclaman@ieee.org>
2017-12-07 08:48:27 -0500

    Update Application.java

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World4";
+        return "Hello Docker World";
     }
 
     public static void main(String[] args) {
